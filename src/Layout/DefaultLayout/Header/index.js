2009136fc67580f01cc8ad4718ef42cc32c55d59
PAT <patdev495@gmail.com>
2023-03-10 02:49:31 +0700

    Design base of the website + header + add tippy package into project

--- src/Layout/DefaultLayout/Header/index.js
+++ src/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,27 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    console.log(images.logo);
+    return (
+        <div className={cx('header')}>
+            <div className={cx('wrapper')}>
+                <img className={cx('logo')} src={images.logo.default} alt="tiktok" />
+                <div className={cx('search')}>
+                    <input className={cx('input')} placeholder="Search account and video" />
+                    <img src={images.search.default} alt="search" />
+                    <span className={cx('horizontal')}></span>
+                    <FontAwesomeIcon icon={faCircleXmark} className={cx('close-icon')} />
+                    <FontAwesomeIcon icon={faSpinner} className={cx('loading-icon')} />
+                </div>
+
+                <div className="action"></div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
--- src/Layout/DefaultLayout/index.js
+++ src/Layout/DefaultLayout/index.js
@@ -1,15 +1,18 @@
 import Header from './Header';
 import SideBar from './SideBar';
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
 
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('containter')}>
                 <SideBar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
-        </>
+        </div>
     );
 }
 
